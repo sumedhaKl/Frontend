c8ca26a57b7f4a6340afb13a609ad20baae208cb
sumedhaKl <sumedhakamaraju@gmail.com>
2023-10-23 20:15:11 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Your JavaScript code here
+    function extendText() {
+        // ...
+    }
+
+    document.getElementById("printButton").addEventListener("click", extendText);
+});
